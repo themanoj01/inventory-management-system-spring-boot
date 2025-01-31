315a41bba02c71cd74d9ea83ee3492dd2ec60b20
Manoj Poudel <februarymanoj2004@gmail.com>
2025-01-31 06:12:23 +0545

    Authorization added in swagger docs

--- src/main/java/com/internship/inventory/inventory_management_system/util/SwaggerConfig.java
+++ src/main/java/com/internship/inventory/inventory_management_system/util/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package com.internship.inventory.inventory_management_system.util;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,6 +28,16 @@ public class SwaggerConfig {
                 .servers(List.of(
                         new Server().url("http://localhost:8082").description("Local Server")
 
-                ));
+                ))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
